692c68ddf762bbea151eb8c35fa12637b427b21b
susheel pal <susheelpal057@gmail.com>
2022-02-13 16:30:25 +0530

    Sunshine weather app

---
+++ app/src/main/java/com/example/sunshine/SettingsActivity.java
@@ -0,0 +1,26 @@
+package com.example.sunshine;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_settings);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
